1fae1fbcd6014a4e04c446a33520d225c9a29f94
NbClKaiZer <105943682+NbClKaiZer@users.noreply.github.com>
2023-06-06 10:28:12 +0200

    add roadmap

---
+++ boardgen.js
@@ -0,0 +1,14 @@
+/*1st iteration:
+set up board
+2nd iteration:
+set up game figures
+3rd iteration:
+add basic controls to snake
+4th iteration:
+add apple spawning
+5th iteration:
+add collision detection
+6th iteration:
+add snake growth
+7th iteration:
+reset condition*/
